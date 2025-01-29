558815f6b7f8a7c7c0ce05d3cae3ea3470dd90e4
Nikita Fomenko <fomenkoper@gmail.com>
2025-01-29 13:44:44 +0300

    change urls

--- stocks_products/logistic/urls.py
+++ stocks_products/logistic/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 from .views import ProductViewSet, StockViewSet, index
+from . import views
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
