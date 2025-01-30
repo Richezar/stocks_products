76e8e9b0360d08b5cd25c18bbacd3329016733d0
Nikita Fomenko <fomenkoper@gmail.com>
2025-01-30 10:19:27 +0300

    change cicd, urls

--- stocks_products/logistic/urls.py
+++ stocks_products/logistic/urls.py
@@ -1,11 +1,8 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, StockViewSet, index
+from .views import ProductViewSet, StockViewSet
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
 
-urlpatterns = [
-    path("", index, name="index")
-]+router.urls
+urlpatterns = router.urls
