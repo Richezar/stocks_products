53cb62adbe3efbdb8f07671fb600da29232fcda8
Nikita Fomenko <fomenkoper@gmail.com>
2025-01-30 10:21:53 +0300

    change urls

--- stocks_products/stocks_products/urls.py
+++ stocks_products/stocks_products/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from stocks_products.logistic.views import index
+from logistic.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
